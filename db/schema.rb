fef1b95eb28608da0da00aa2a995c55060cd4838
GuillaumeM59 <manier-guillaume@orange.fr>
2016-04-18 11:05:16 +0200

    brands migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416180856) do
+ActiveRecord::Schema.define(version: 20160418085100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brands", force: :cascade do |t|
+    t.string   "name"
+    t.string   "category"
+    t.string   "brandpic"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
