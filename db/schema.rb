3a98756a1af2f3fbc0b8c1b81ede11f8aedb9aca
GuillaumeM59 <manier-guillaume@orange.fr>
2016-04-18 11:27:22 +0200

    add Shop model, scaffold and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418085100) do
+ActiveRecord::Schema.define(version: 20160418091753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20160418085100) do
     t.string   "brandpic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "shops", force: :cascade do |t|
+    t.integer  "brand_id_id"
+    t.string   "adress"
+    t.string   "zipcode"
+    t.string   "city"
+    t.boolean  "isdrive"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
