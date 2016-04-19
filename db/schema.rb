ce56dff6ecd30d955220a8a0dc47a99bf0c55ed5
GuillaumeM59 <manier-guillaume@orange.fr>
2016-04-19 15:54:33 +0200

    add cities model and migrate it

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418101629) do
+ActiveRecord::Schema.define(version: 20160419135334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 20160418101629) do
     t.string   "carmodel"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "region"
+    t.string   "departement"
+    t.string   "zip"
+    t.string   "name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "feedbacks", force: :cascade do |t|
