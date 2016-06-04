a34dc8970d82797cf9b894a76700dd694b1d9996
GuillaumeM59 <manier-guillaume@orange.fr>
2016-06-05 01:33:31 +0200

    add car to user, update user signin

--- app/controllers/carmodels_controller.rb
+++ app/controllers/carmodels_controller.rb
@@ -69,6 +69,6 @@ class CarmodelsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def carmodel_params
-      params.require(:carmodel).permit(:brand_id, :name)
+      params.require(:carmodel).permit(:brand_id, :name, :year)
     end
 end
